f44c3525ea94af629e0feab6048bb88828038b0d
lipeyamane@gmail.com <lipeyamane@gmail.com>
2020-11-02 21:37:44 -0300

    Commit de finalização dos arquivos

--- download_concorrente.py
+++ download_concorrente.py
@@ -5,27 +5,23 @@
 
 from wget import download #Import apenas da função de download da biblioteca wget
 from time import time #Import apenas da função time da biblioteca time que será usado para calcular tempo de download
-from threading import Thread
-from threading import active_count
-
+from threading import Thread #Import da função Thread da biblioteca threading
+from threading import active_count #Import da função active_count da biblioteca threading, que verificará quantos processos estão em execução para a correta finalização do temporizador
 
 
 endereco = 'http://arquivos.afonsomiguel.com/' #String com o endereço de hospedagem dos arquivos na web
-num_arquivo = '' #String que será iterada com o número do arquivo sequêncial #estensão do arquivo
-arquivo = ''
 
 
 inicio = time() #Variável que armazena o tempo inicial da execução
 
 
 def download_img(arquivo):
-    download(f'{endereco}arquivo_{arquivo}.jpg', f'arquivo_{arquivo}.jpg')
-    print('arquivo...')
+    download(f'{endereco}arquivo_{arquivo}.jpg', f'arquivo_{arquivo}.jpg') #Função que executa o download dos arquivos e os nomeia sequêncialmente
+
 
 for i in range(0,10):
-    num_arquivo = str(i)
     arquivo = (f'arquivo_{i}.jpg')
-    Thread(target=download_img, args=[i]).start()
+    Thread(target=download_img, args=[i]).start() #Laço iterador que chama a função Thread a executando até que os 10 arquivos sejam baixados.
 
 
 while True:
--- download_sequencial.py
+++ download_sequencial.py
@@ -8,18 +8,17 @@ from time import time #Import apenas da função time da biblioteca time que ser
 
 
 endereco = 'http://arquivos.afonsomiguel.com/' #String com o endereço de hospedagem dos arquivos na web
-arquivo_string = 'arquivo_' #String com a primeira parte do nome do arquivo
 num_arquivo = '' #String que será iterada com o número do arquivo sequêncial
-extensao = '.jpg' #estensão do arquivo
 
 
 inicio = time() #Variável que armazena o tempo inicial da execução
 
 for i in range(0,10): #Estrutura de repetição para execução do download de todos os arquivos
     num_arquivo = str(i) #Conversão de numero inteiro para string para que na sequência seja concatenado de forma correta
-    download(endereco+arquivo_string+num_arquivo+extensao, arquivo_string+num_arquivo+extensao)
+    download(f'{endereco}arquivo_{num_arquivo}.jpg', f'arquivo_{num_arquivo}.jpg')
+    print(f'\nBaixando arquivo_{num_arquivo}.jpg .....\n')
 
 fim = time() #Variável que armazena o tempo final da execução
-print('\n--------------------------------------------------\n')
+print('\n-----------------------------------------------------\n')
 print(f'\nTempo de Download das {i+1} Imagens foi de {fim-inicio:.3} Segundos') #Impressão do tempo total de execução
-print('\n--------------------------------------------------')
+print('\n-----------------------------------------------------')
